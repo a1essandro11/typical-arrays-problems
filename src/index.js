dea8ef93b8997091fc913e9ca2068aa710721042
Alexandr <ans11081990@gmail.com>
2020-03-21 22:12:02 +0300

    init: add index.js file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
+exports.min = function min(array) {
+    if (typeof array === 'undefined' || array.length === 0) return 0;
 
-exports.min = function min (array) {
-  return 0;
+    let minimum = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        if (minimum > array[i]) {
+            minimum = array[i];
+        }
+    }
+    return minimum;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (typeof array === 'undefined' || array.length === 0) return 0;
+    let maximum = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        if (maximum < array[i]) {
+            maximum = array[i];
+        }
+    }
+    return maximum;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array == undefined || array.length == 0) return 0;
+    let sum = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        sum = sum + array[i];
+
+    }
+    return sum / array.length;
 }
+
+
